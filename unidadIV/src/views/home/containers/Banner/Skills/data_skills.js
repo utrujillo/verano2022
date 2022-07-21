063f2f10c687a69b75d56ebefb3db691a0279a96
Uziel Trujillo <uziel.trujillo.mx@cloudappi.net>
2022-07-21 13:21:52 -0500

    Skills con javascript 	new file:   src/views/home/containers/Banner/Skills/data_skills.js 	modified:   src/views/home/containers/Banner/Skills/index.js

---
+++ unidadIV/src/views/home/containers/Banner/Skills/data_skills.js
@@ -0,0 +1,29 @@
+const data_skills = [
+  {
+    id: 1,
+    habilidad: 'Programación',
+    porcentaje: 100
+  },
+  {
+    id: 2,
+    habilidad: 'Diseño',
+    porcentaje: 70
+  },
+  {
+    id: 3,
+    habilidad: 'APIs',
+    porcentaje: 85
+  },
+  {
+    id: 4,
+    habilidad: 'Documentación',
+    porcentaje: 90
+  },
+  {
+    id: 5,
+    habilidad: 'x',
+    porcentaje: 50
+  },
+]
+
+export default data_skills
--- unidadIV/src/views/home/containers/Banner/Skills/index.js
+++ unidadIV/src/views/home/containers/Banner/Skills/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import classes from './Skills.module.css'
+import data_skills from './data_skills'
+import classes from './Skills.module.css' 
 
 const Skills = () => {
   return (
     <div className={`${classes.skills} opensans fs_14`}>
       <div className={classes.skills_grid}>
-        <div className={classes.bar} style={{ "width": "100%" }}></div>
-        <div>Programacion</div>
-        <div className={classes.bar} style={{ "width": "80%" }}></div>
-        <div>Disenho</div>
-        <div className={classes.bar} style={{ "width": "90%" }}></div>
-        <div>APIs</div>
-        <div className={classes.bar} style={{ "width": "85%" }}></div>
-        <div>Documentacion</div>
+        {
+          data_skills.map( skill => {
+            return(
+              <>
+                <div
+                  className={classes.bar}
+                  style={{ "width": `${skill.porcentaje}%` }}
+                ></div>
+                <div>{ skill.habilidad }</div>
+              </>
+            )
+          } )
+        }
       </div>
     </div>
   )
