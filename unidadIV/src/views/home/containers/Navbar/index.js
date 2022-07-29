85d208b7ca7bfe87049676397243354bc897987a
Uziel Trujillo <uziel.trujillo.mx@cloudappi.net>
2022-07-29 13:45:47 -0500

    Mapeando datos desde Strapi 	modified:   unidadIV/src/views/home/containers/Navbar/index.js 	modified:   unidadV/.tmp/data.db 	modified:   unidadV/src/extensions/documentation/documentation/1.0.0/full_documentation.json 	modified:   unidadV/src/extensions/documentation/public/index.html

--- unidadIV/src/views/home/containers/Navbar/index.js
+++ unidadIV/src/views/home/containers/Navbar/index.js
@@ -6,7 +6,7 @@ import './navbar.css'
 const Navbar = (props) => {
   
   const { data } = props
-  console.log( data )
+  // console.log( data )
 
   return (
     <div class="wrapper_navbar">
@@ -15,12 +15,11 @@ const Navbar = (props) => {
       </div>
       <div class="navbar_menu opensans">
         <ul id="navigation">
-          <li className='fs_48'>Inicio</li>
-          <li>Habilidades</li>
-          <li>Trabajo</li>
-          <li>Estrategia</li>
-          <li><Text title='Soluciones' css_styles='fs_20 fw_bold' /></li>
-          <li><Text title='Hola' /></li>
+          {
+            data?.menu?.map( item => (
+              <li>{ item?.texto }</li>
+            ) )
+          }
         </ul>
       </div>
       <div class="navbar_buttons">
